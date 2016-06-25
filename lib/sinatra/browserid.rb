b38227002824ad7f84b3b47a8324e212e0991ee6
onli <onli@paskuda.biz>
2016-06-25 21:22:30 +0200

    Implement signature verification, update info

--- lib/sinatra/browserid.rb
+++ lib/sinatra/browserid.rb
@@ -1,8 +1,9 @@
 #!/usr/bin/env ruby
 
 require "open-uri"
-require 'json/jwt'
-require 'json/jwk'
+require 'json'
+require 'url_safe_base64'
+require 'jwt'
 require "sinatra/base"
 require 'sinatra/browserid/helpers'
 require 'sinatra/browserid/template'
@@ -28,19 +29,19 @@ module Sinatra
               begin
                   # 3. Server checks signature
                   # for that, fetch the public key from the LA instance (TODO: Do that beforehand for trusted instances, and generally cache the key)
-                  public_key_jwks = URI.parse(URI.escape(settings.browserid_url + '/jwks.json')).read
-                  public_key = JSON::JWK.new(public_key_jwks)
-
-                  # TODO: we are skipping verification here, because the JWT gem is throwing an error, calling verify! on a string, which of course does not have this method. This is possibly a bug in the JWT module and needs to be fixed, or signature verification done manually
-                  id_token = JSON::JWT.decode params[:id_token], :skip_verification
+                  public_key_jwks = JSON.parse(URI.parse(URI.escape(settings.browserid_url + '/jwks.json')).read)
+                  public_key = OpenSSL::PKey::RSA.new
+                  public_key.e = OpenSSL::BN.new UrlSafeBase64.decode64(public_key_jwks["keys"][0]["e"]), 2 
+                  public_key.n = OpenSSL::BN.new UrlSafeBase64.decode64(public_key_jwks["keys"][0]["n"]), 2
+                  
+                  id_token = JWT.decode params[:id_token], public_key, true, { :algorithm => 'RS256' }
+                  id_token = id_token[0]
                   # 4. Needs to make sure token is still valid
-                  if (# id_token.verify! public_key  
-                      id_token[:iss] == settings.browserid_url &&
-                      id_token[:aud] == request.base_url.chomp('/') &&        
-                      # id_token[:sub].present? &&  TODO: Decide whether we really can skip these two tests
-                      # id_token[:nonce] == expected_nonce &&
-                      id_token[:exp] > Time.now.to_i)
-                          session[:browserid_email] = id_token[:email_verified]
+                  if (id_token["iss"] == settings.browserid_url &&
+                      id_token["aud"] == request.base_url.chomp('/') &&        
+                      id_token["exp"] > Time.now.to_i &&
+                      id_token["email_verified"])
+                          session[:browserid_email] = id_token["email"]
                           redirect "/"
                   end
               rescue OpenURI::HTTPError => e
